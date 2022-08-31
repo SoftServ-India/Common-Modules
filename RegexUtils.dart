2766dc2dba17ff0684fa9d5be0661a7352baabc5
SanwalSoftServ <106134563+SanwalSoftServ@users.noreply.github.com>
2022-08-31 11:44:58 +0530

    Code added

--- RegexUtils.dart
+++ RegexUtils.dart
@@ -1,3 +1,10 @@
+/*
+* Below the some common regular expersion which is use to validate the Input fields
+* 
+* 
+* */
+
+
 final emailPattern = RegExp(
     r'^(([^<>()[\]\\...,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
 
